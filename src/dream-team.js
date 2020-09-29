1629c371044b5ba4154fa384ae1d50f1dbc8b09e
Valeriy Osipov <freebs@mail.ru>
2020-09-29 16:19:31 +0400

    fix: added validation for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,11 @@
 module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) {
+      return false;  
+  }
   let isWrong = true;
   let firstLetters = [];
   for (let i = 0; i < members.length; i++) {
-      if (typeof(members[i]) == "string"){
+      if (typeof(members[i]) === "string"){
           firstLetters.push(members[i].trim().slice(0,1).toUpperCase());
           isWrong = false;
       }
