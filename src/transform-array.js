fd522019f2765cc0d0e611875e4471837dc29842
Valeriy Osipov <freebs@mail.ru>
2020-09-29 18:01:36 +0400

    fix: fixed transorm-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,27 +3,29 @@ module.exports = function transform(arr){
       throw "Error";
   }
   let answer = [];
+  let flag;
   for (let i = 0; i < arr.length; i++) {
       if (arr[i] === "--discard-next") {
           if (i !== arr.length - 1) {
               i++;
+              flag = true;
           }
       } else if (arr[i] === "--double-next") {
           if (i !== arr.length - 1) {
               answer.push(arr[i + 1]);
           }
       } else if (arr[i] === "--discard-prev") {
-          if (i !== 0) {
+          if (i !== 0 && !flag) {
               answer.pop();
           }
       } else if (arr[i] === "--double-prev") {
-          if (i !== 0) {
-              answer.push(arr[i+1]);
+          if (i !== 0 && !flag) {
+              answer.push(arr[i-1]);
           }
       } else {
           answer.push(arr[i]);
+          flag = false;
       }
-
   }
   return answer;
 }
