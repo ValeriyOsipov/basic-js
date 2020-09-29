7a38359426e9e0b2ad2ec6102a8a5067a467680a
Valeriy Osipov <freebs@mail.ru>
2020-09-29 16:09:14 +0400

    fix: corrected validation in carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 module.exports = function dateSample(sampleActivity) {
   const halfLifePeriod = 5730;
   const modernActivity = 15;
-  if (isNaN(parseFloat(sampleActivity)) || parseFloat(sampleActivity) > modernActivity || parseFloat(sampleActivity) < 0) {
+  if (typeof(sampleActivity) !== "string" || isNaN(parseFloat(sampleActivity)) || parseFloat(sampleActivity) > modernActivity || parseFloat(sampleActivity) <= 0) {
       return false;
   }
   return Math.ceil(Math.log(modernActivity / parseFloat(sampleActivity))/(0.693 / halfLifePeriod));
